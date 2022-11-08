8b9d5043f211b676de93a528baee5b74ed39abf8
silksonjohn84 <117662446+silksonjohn84@users.noreply.github.com>
2022-11-09 05:03:52 +0530

    Update test.java

--- src/gitproject/test.java
+++ src/gitproject/test.java
@@ -2,11 +2,12 @@ package gitproject;
 
 public class test {
 	public static void main(String[] args) {
-		int i,j;
+		int i,j,k;
 		for(i=1; i<=4 ; i++) {
 			for(j=1; j<=3; j++)
 				System.out.print(i);
 			System.out.println();
+			k=i+j
 
 }
 	}
